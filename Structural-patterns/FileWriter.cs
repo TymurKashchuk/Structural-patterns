fc7ecee77eb80d8e59cdd91a282a37335b3ced7a
TymurKashchuk <t1mu4337@gmail.com>
2026-03-19 19:59:21 +0200

    adapter done

---
+++ Structural-patterns/FileWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural_patterns
+{
+    public class FileWriter
+    {
+        private string _filePath;
+        public FileWriter(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public void Write(string text)
+        {
+            File.AppendAllText(_filePath, text);
+        }
+        public void WriteLine(string text)
+        {
+            File.AppendAllText(_filePath, text + "\n");
+        }
+    }
+}
---
+++ Structural-patterns/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Structural_patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger consoleLogger = new Logger();
+            consoleLogger.Log("LOG");
+            consoleLogger.Warn("Warn");
+            consoleLogger.Error("Error");
+
+            FileWriter fileWriter = new FileWriter("log.txt");
+            FileLoggerAdapter fileLogger = new FileLoggerAdapter(fileWriter);
+            fileLogger.Log("file Log");
+            fileLogger.Warn("file Warn");
+            fileLogger.Error("file Error");
+        }
+    }
+}
